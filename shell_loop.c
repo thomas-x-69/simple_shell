8b1f0c9473d11e118f0c47f6b31c31ccbf220872
thomas-x-69 <thomas.ashraf975@gmail.com>
2023-12-13 20:57:35 +0200

    cc

--- shell_loop.c
+++ shell_loop.c
@@ -15,31 +15,47 @@ int hsh(info_t *info, char **av)
 	while (r != -1 && builtin_ret != -2)
 	{
 		clear_info(info);
+
+		// Display prompt if in interactive mode
 		if (interactive(info))
 			_puts("$ ");
 		_eputchar(BUF_FLUSH);
+
+		// Get user input
 		r = get_input(info);
+
 		if (r != -1)
 		{
 			set_info(info, av);
+
+			// Check for and execute built-in commands
 			builtin_ret = find_builtin(info);
 			if (builtin_ret == -1)
 				find_cmd(info);
 		}
 		else if (interactive(info))
 			_putchar('\n');
+
+		// Free allocated memory
 		free_info(info, 0);
 	}
+
+	// Write command history to file and free remaining memory
 	write_history(info);
 	free_info(info, 1);
+
+	// Exit with status code in non-interactive mode
 	if (!interactive(info) && info->status)
 		exit(info->status);
+
+	// Handle exit due to built-in command
 	if (builtin_ret == -2)
 	{
 		if (info->err_num == -1)
 			exit(info->status);
 		exit(info->err_num);
 	}
+
 	return (builtin_ret);
 }
 
@@ -48,9 +64,9 @@ int hsh(info_t *info, char **av)
  * @info: the parameter & return info struct
  *
  * Return: -1 if builtin not found,
- *			0 if builtin executed successfully,
- *			1 if builtin found but not successful,
- *			-2 if builtin signals exit()
+ *         0 if builtin executed successfully,
+ *         1 if builtin found but not successful,
+ *        -2 if builtin signals exit()
  */
 int find_builtin(info_t *info)
 {
@@ -67,13 +83,20 @@ int find_builtin(info_t *info)
 		{NULL, NULL}
 	};
 
+	// Iterate through the built-in command table
 	for (i = 0; builtintbl[i].type; i++)
+	{
+		// Check if the input command matches a built-in command
 		if (_strcmp(info->argv[0], builtintbl[i].type) == 0)
 		{
 			info->line_count++;
+
+			// Execute the built-in command function
 			built_in_ret = builtintbl[i].func(info);
 			break;
 		}
+	}
+
 	return (built_in_ret);
 }
 
@@ -89,17 +112,17 @@ void find_cmd(info_t *info)
 	int i, k;
 
 	info->path = info->argv[0];
-	if (info->linecount_flag == 1)
-	{
-		info->line_count++;
-		info->linecount_flag = 0;
-	}
+
+	// Count the number of non-delimiter characters in the command
 	for (i = 0, k = 0; info->arg[i]; i++)
 		if (!is_delim(info->arg[i], " \t\n"))
 			k++;
+
+	// If no non-delimiter characters, return
 	if (!k)
 		return;
 
+	// Find the command in the PATH environment variable
 	path = find_path(info, _getenv(info, "PATH="), info->argv[0]);
 	if (path)
 	{
@@ -108,8 +131,8 @@ void find_cmd(info_t *info)
 	}
 	else
 	{
-		if ((interactive(info) || _getenv(info, "PATH=")
-			|| info->argv[0][0] == '/') && is_cmd(info, info->argv[0]))
+		// Check if the command is an executable in the current directory
+		if ((interactive(info) || _getenv(info, "PATH=") || info->argv[0][0] == '/') && is_cmd(info, info->argv[0]))
 			fork_cmd(info);
 		else if (*(info->arg) != '\n')
 		{
@@ -129,27 +152,36 @@ void fork_cmd(info_t *info)
 {
 	pid_t child_pid;
 
+	// Fork a child process
 	child_pid = fork();
 	if (child_pid == -1)
 	{
-		/* TODO: PUT ERROR FUNCTION */
+		// TODO: Error handling
 		perror("Error:");
 		return;
 	}
+
+	// Code executed by the child process
 	if (child_pid == 0)
 	{
+		// Execute the command using execve
 		if (execve(info->path, info->argv, get_environ(info)) == -1)
 		{
+			// Handle execution failure
 			free_info(info, 1);
 			if (errno == EACCES)
 				exit(126);
 			exit(1);
 		}
-		/* TODO: PUT ERROR FUNCTION */
+		// TODO: Error handling
 	}
+	// Code executed by the parent process
 	else
 	{
+		// Wait for the child process to finish
 		wait(&(info->status));
+
+		// Check if the child process terminated normally
 		if (WIFEXITED(info->status))
 		{
 			info->status = WEXITSTATUS(info->status);
--- string1.c
+++ string1.c
@@ -9,23 +9,16 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	/* Initialize index variable */
 	int i = 0;
 
-	/* Check if source and destination are the same or if source is NULL */
 	if (dest == src || src == 0)
 		return (dest);
-
-	/* Copy each character from source to destination */
 	while (src[i])
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	
-	/* Add null terminator to the destination string */
 	dest[i] = 0;
-
 	return (dest);
 }
 
@@ -37,29 +30,18 @@ char *_strcpy(char *dest, char *src)
  */
 char *_strdup(const char *str)
 {
-	/* Initialize variables for length and return string */
 	int length = 0;
 	char *ret;
 
-	/* Check if the input string is NULL */
 	if (str == NULL)
 		return (NULL);
-
-	/* Calculate the length of the input string */
 	while (*str++)
 		length++;
-
-	/* Allocate memory for the duplicated string */
 	ret = malloc(sizeof(char) * (length + 1));
-
-	/* Check if memory allocation is successful */
 	if (!ret)
 		return (NULL);
-
-	/* Copy characters from the input string to the duplicated string */
 	for (length++; length--;)
 		ret[length] = *--str;
-
 	return (ret);
 }
 
@@ -71,17 +53,12 @@ char *_strdup(const char *str)
  */
 void _puts(char *str)
 {
-	/* Initialize index variable */
 	int i = 0;
 
-	/* Check if the input string is NULL */
 	if (!str)
 		return;
-
-	/* Loop through the string and print each character */
 	while (str[i] != '\0')
 	{
-		/* Call _putchar to print each character */
 		_putchar(str[i]);
 		i++;
 	}
@@ -96,21 +73,15 @@ void _puts(char *str)
  */
 int _putchar(char c)
 {
-	/* Initialize static variables for buffer and index */
 	static int i;
 	static char buf[WRITE_BUF_SIZE];
 
-	/* Check if buffer is full or if BUF_FLUSH character is encountered */
 	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
 	{
-		/* Write the buffer to stdout and reset the index */
 		write(1, buf, i);
 		i = 0;
 	}
-
-	/* Check if character is not BUF_FLUSH, then add it to the buffer */
 	if (c != BUF_FLUSH)
 		buf[i++] = c;
-
 	return (1);
 }
