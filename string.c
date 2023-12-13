ded9d85f2c428c3e1babc2c4d9c22b4f88f7b18e
thomas-x-69 <thomas.ashraf975@gmail.com>
2023-12-13 21:11:39 +0200

    cc

--- string.c
+++ string.c
@@ -4,45 +4,36 @@
  * _strlen - returns the length of a string
  * @s: the string whose length to check
  *
- * Return: integer length of the string
+ * Return: integer length of string
  */
 int _strlen(char *s)
 {
 	int i = 0;
 
-	// Check if the string is NULL
 	if (!s)
 		return (0);
 
-	// Iterate through the string characters
 	while (*s++)
 		i++;
-
 	return (i);
 }
 
 /**
- * _strcmp - performs lexicographic comparison of two strings.
- * @s1: the first string
- * @s2: the second string
+ * _strcmp - performs lexicogarphic comparison of two strangs.
+ * @s1: the first strang
+ * @s2: the second strang
  *
  * Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
  */
 int _strcmp(char *s1, char *s2)
 {
-	// Iterate through the characters of both strings
 	while (*s1 && *s2)
 	{
-		// Compare corresponding characters
 		if (*s1 != *s2)
 			return (*s1 - *s2);
-
-		// Move to the next character
 		s1++;
 		s2++;
 	}
-
-	// Check if both strings are equal
 	if (*s1 == *s2)
 		return (0);
 	else
@@ -54,16 +45,13 @@ int _strcmp(char *s1, char *s2)
  * @haystack: string to search
  * @needle: the substring to find
  *
- * Return: address of the next character of haystack or NULL
+ * Return: address of next char of haystack or NULL
  */
 char *starts_with(const char *haystack, const char *needle)
 {
-	// Iterate through the characters of both strings
 	while (*needle)
 		if (*needle++ != *haystack++)
 			return (NULL);
-
-	// Return the address of the next character of haystack
 	return ((char *)haystack);
 }
 
@@ -72,23 +60,16 @@ char *starts_with(const char *haystack, const char *needle)
  * @dest: the destination buffer
  * @src: the source buffer
  *
- * Return: pointer to the destination buffer
+ * Return: pointer to destination buffer
  */
 char *_strcat(char *dest, char *src)
 {
 	char *ret = dest;
 
-	// Move to the end of the destination buffer
 	while (*dest)
 		dest++;
-
-	// Copy characters from the source buffer to the destination buffer
 	while (*src)
 		*dest++ = *src++;
-
-	// Add the null terminator to the destination buffer
 	*dest = *src;
-
-	// Return a pointer to the destination buffer
 	return (ret);
 }
