c5004f98f8771d046d9f5f6f4b3a5e4a0ce04cec
thomas-x-69 <thomas.ashraf975@gmail.com>
2023-12-13 20:29:19 +0200

    changes 9

--- errors.c
+++ errors.c
@@ -1,22 +1,25 @@
 #include "shell.h"
 
 /**
- *_eputs - prints an input string
+ * _eputs - prints an input string to stderr
  * @str: the string to be printed
  *
  * Return: Nothing
  */
 void _eputs(char *str)
 {
-	int i = 0;
+    int i = 0;
 
-	if (!str)
-		return;
-	while (str[i] != '\0')
-	{
-		_eputchar(str[i]);
-		i++;
-	}
+    /* Check if the string is not NULL */
+    if (!str)
+        return;
+
+    /* Iterate through the string and write each character to stderr */
+    while (str[i] != '\0')
+    {
+        _eputchar(str[i]);
+        i++;
+    }
 }
 
 /**
@@ -28,58 +31,70 @@ void _eputs(char *str)
  */
 int _eputchar(char c)
 {
-	static int i;
-	static char buf[WRITE_BUF_SIZE];
+    static int i;
+    static char buf[WRITE_BUF_SIZE];
 
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
-	{
-		write(2, buf, i);
-		i = 0;
-	}
-	if (c != BUF_FLUSH)
-		buf[i++] = c;
-	return (1);
+    /* Check if it's time to flush the buffer or if the buffer is full */
+    if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+    {
+        write(2, buf, i);
+        i = 0;
+    }
+
+    /* Check if c is not BUF_FLUSH and add it to the buffer */
+    if (c != BUF_FLUSH)
+        buf[i++] = c;
+
+    return (1);
 }
 
 /**
- * _putfd - writes the character c to given fd
+ * _putfd - writes the character c to the given file descriptor
  * @c: The character to print
- * @fd: The filedescriptor to write to
+ * @fd: The file descriptor to write to
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _putfd(char c, int fd)
 {
-	static int i;
-	static char buf[WRITE_BUF_SIZE];
+    static int i;
+    static char buf[WRITE_BUF_SIZE];
 
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
-	{
-		write(fd, buf, i);
-		i = 0;
-	}
-	if (c != BUF_FLUSH)
-		buf[i++] = c;
-	return (1);
+    /* Check if it's time to flush the buffer or if the buffer is full */
+    if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+    {
+        write(fd, buf, i);
+        i = 0;
+    }
+
+    /* Check if c is not BUF_FLUSH and add it to the buffer */
+    if (c != BUF_FLUSH)
+        buf[i++] = c;
+
+    return (1);
 }
 
 /**
- *_putsfd - prints an input string
+ * _putsfd - prints an input string to the given file descriptor
  * @str: the string to be printed
- * @fd: the filedescriptor to write to
+ * @fd: the file descriptor to write to
  *
- * Return: the number of chars put
+ * Return: the number of characters written
  */
 int _putsfd(char *str, int fd)
 {
-	int i = 0;
+    int i = 0;
 
-	if (!str)
-		return (0);
-	while (*str)
-	{
-		i += _putfd(*str++, fd);
-	}
-	return (i);
+    /* Check if the string is not NULL */
+    if (!str)
+        return (0);
+
+    /* Iterate through the string and write each character to the given file descriptor */
+    while (*str)
+    {
+        i += _putfd(*str++, fd);
+    }
+
+    return (i);
 }
