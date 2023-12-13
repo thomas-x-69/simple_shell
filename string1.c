4a6e68fa6b8dcb4225cdbfe98f039ad4441a6dd3
thomas-x-69 <thomas.ashraf975@gmail.com>
2023-12-13 19:25:42 +0200

    ccc

--- string1.c
+++ string1.c
@@ -1,34 +1,32 @@
 #include "shell.h"
 
 /**
- * _strcpy - Copies a string.
- * @dest: The destination buffer.
- * @src: The source string.
+ * _strcpy - copies a string
+ * @dest: the destination
+ * @src: the source
  *
- * Return: Pointer to destination.
+ * Return: pointer to destination
  */
-char *_strcpy(char *dest, const char *src)
+char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	if (dest == src || src == NULL)
+	if (dest == src || src == 0)
 		return (dest);
-
 	while (src[i])
 	{
 		dest[i] = src[i];
 		i++;
 	}
-
-	dest[i] = '\0';
+	dest[i] = 0;
 	return (dest);
 }
 
 /**
- * _strdup - Duplicates a string.
- * @str: The string to duplicate.
+ * _strdup - duplicates a string
+ * @str: the string to duplicate
  *
- * Return: Pointer to the duplicated string.
+ * Return: pointer to the duplicated string
  */
 char *_strdup(const char *str)
 {
@@ -48,18 +46,17 @@ char *_strdup(const char *str)
 }
 
 /**
- * _puts - Prints a string to stdout.
- * @str: The string to be printed.
+ *_puts - prints an input string
+ *@str: the string to be printed
  *
- * Return: Nothing.
+ * Return: Nothing
  */
-void _puts(const char *str)
+void _puts(char *str)
 {
 	int i = 0;
 
 	if (!str)
 		return;
-
 	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
@@ -68,8 +65,8 @@ void _puts(const char *str)
 }
 
 /**
- * _putchar - Writes a character to stdout.
- * @c: The character to print.
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
@@ -84,9 +81,7 @@ int _putchar(char c)
 		write(1, buf, i);
 		i = 0;
 	}
-
 	if (c != BUF_FLUSH)
 		buf[i++] = c;
-
 	return (1);
 }
